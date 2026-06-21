c3a1df05f2de82629cdd65ac1c4885e413ed9bef
0x01 <143021151+VedantKhapre@users.noreply.github.com>
2026-06-21 16:07:03 +0530

    expression

---
+++ codeforces/level-2/8-expression.cpp
@@ -0,0 +1,23 @@
+#define ll long long
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    FAST_IO;
+
+    ll a, b, c;
+    cin >> a >> b >> c;
+
+    ll ans = a + b + c;
+    ans = max(ans, (a + b) * c);
+    ans = max(ans, a * (b + c));
+    ans = max(ans, a * b * c);
+
+    cout << ans << endl;
+    
+    return 0;
+}
