eb72039a6d996351ddc8c328ab9b75f636946a6c
0x01 <143021151+VedantKhapre@users.noreply.github.com>
2026-06-21 16:31:10 +0530

    anton and letters

---
+++ codeforces/level-1/10-anton-and-letters.cpp
@@ -0,0 +1,26 @@
+#include <charconv>
+#include <string>
+#define ll long long
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    FAST_IO;
+
+    string letters;
+    getline(cin, letters);
+
+    set <char> unique; 
+    for(ll i = 1; i < letters.length() - 1; i++){
+        if(letters[i] != ',' && letters[i] != ' '){
+            unique.insert(letters[i]);
+        }
+    }
+    cout << unique.size() << endl;
+    
+    return 0;
+}
